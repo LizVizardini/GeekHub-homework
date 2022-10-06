01e5dafdda5ec57277081c3c7af9458cf4113f93
LizVizardini <47724644+LizVizardini@users.noreply.github.com>
2022-10-06 08:57:42 -0400

    third commit

--- HT_2/django_project_name/app_name/settings.py
+++ HT_2/django_project_name/app_name/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1#lh_02#hi6^134-t0tdo-etz&^f(1#z-q8p5q3ck%!1zd365#'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
